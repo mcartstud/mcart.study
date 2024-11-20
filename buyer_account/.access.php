3ab975cdc43690d28af47e7dc2869261455b25a5
enteryourponyname <YouTubebogdan@mail.ru>
2024-11-20 15:31:58 +0300

    исправление ошибок доступа

--- buyer_account/.access.php
+++ buyer_account/.access.php
@@ -1,7 +1,8 @@
 <?
+$PERM["favourites"]["8"]="W";
+$PERM["favourites"]["6"]="W";
+$PERM["favourites"]["9"]="W";
 $PERM["favourites"]["*"]="D";
-$PERM["favourites"]["G8"]="W";
-$PERM["favourites"]["G6"]="W";
 $PERM["index.php"]["G6"]="W";
 $PERM["index.php"]["G8"]="W";
 ?>
