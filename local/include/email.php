60f169a979247a5c359f28ed8712ce80c9c49ce7
enteryourponyname <YouTubebogdan@mail.ru>
2024-11-27 19:50:17 +0300

    Правка включаемых областей

--- local/include/email.php
+++ local/include/email.php
@@ -1,2 +1,2 @@
-<a href="info@domain.com"><span class="text-black fl-bigmug-line-email64"></span> <span
+<a href="mailto:info@domain.com"><span class="text-black fl-bigmug-line-email64"></span> <span
                   class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
--- local/include/phone.php
+++ local/include/phone.php
@@ -1,1 +1,1 @@
-<a href="+2 102 3923 3922" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
+<a href="tel:+2 102 3923 3922" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
